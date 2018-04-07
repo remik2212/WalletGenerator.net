677097764cbd88c6f94755a6f65e160ba9b0af3d
Todd Farrell <todd@1251.org>
2018-04-07 14:59:40 +1000

    Add support for Pigeoncoin (http://pigeoncoin.org)

--- src/janin.currency.js
+++ src/janin.currency.js
@@ -232,6 +232,7 @@ janin.currencies = [
     janin.currency.createCurrency ("Pesetacoin",          0x2f, 0xaf, "6",    "[ST]" , "L6qoz2SQN6U9vGNoST35QP85PQbg4s5rDn"),
     janin.currency.createCurrency ("PHCoin",              0x37, 0xb7, "7",    "U"    , "P9e6c714JUHUfuBVHSS36eqaxGCN6X8nyU"),
     janin.currency.createCurrency ("PhoenixCoin",         0x38, 0xb8, "7",    "U"    , "PsaaD2mLfAPUJXhMYdC1DBavkJhZj14k6X"),
+    janin.currency.createCurrency ("Pigeoncoin",          0x37, 0x80, "5",    "[LK]" , "PEkuhX8MdZJa1WX3YjgyR47jEYadXPVxQg"),
     janin.currency.createCurrency ("PiggyCoin",           0x76, 0xf6, "9",    "d"    , "pqXotCKo6mmtYtLY5mi9uEW22mPFgKoLvx"),
     janin.currency.createCurrency ("Pinkcoin",            0x3,  0x83, "[RQP]","L"    , "2Xgy8K2n5cVmnm8Se2rDojQ1GdfHdktx8r"),
     janin.currency.createCurrency ("PIVX",                0x1e, 0xd4, "8",    "Y"    , "DSiCurCzgdzqSP1urFg3VZJfrpyhMWjEAp"),
